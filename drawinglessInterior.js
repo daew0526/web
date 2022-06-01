4c0fa3aa5307850064b62b3baab1370d9af33ab0
daew0526 <101858460+daew0526@users.noreply.github.com>
2022-06-01 22:39:28 +0900

    0601

--- drawinglessInterior.js
+++ drawinglessInterior.js
@@ -16,7 +16,6 @@ let sx, sy;                  // 드래그 시작점
 let ex, ey;                  // 드래그 끝점
 let color;                   // 현재 색상
 let moving = -1;             // 이동중인 도형 첨자
-let backup;
 
 let squaredrawing;           // 그리고 있는 중인가
 let circledrawing;
@@ -72,13 +71,15 @@ function stModeClick() {
 function sqEraserClick() {
 	arSquare.pop();
 	drawRects();
-}
-function stEraserClick() {
-	arStraight.pop();
-	drawRects();
+	console.log(arSquare);
 }
 function scEraserClick() {
 	arCircle.pop();
+	drawRects();
+	console.log(arCircle);
+}
+function stEraserClick() {
+	arStraight.pop();
 	drawRects();
 }
 //사각형 생성자
@@ -108,9 +109,25 @@ function Straight(sx, sy, ex, ey, color) {
 // x, y 위치의 사각형 찾음. 없으면 -1
 function getSquare(x, y) {
 	for (let i = 0; i < arSquare.length; i++) {
-		let rect = arSquare[i];
-		if (x > rect.sx && x < rect.ex && y > rect.sy && y < rect.ey) {
+		let r = arSquare[i];
+		if (x > r.sx && x < r.ex && y > r.sy && y < r.ey) {
 			return i;
+		}
+	}
+	return -1;
+}
+function getCircle(x, y) {
+	for (let i = 0; i < arCircle.length; i++) {
+		let r = arCircle[i];
+		if (r.ex >= r.sx) {
+			if (x < r.ex && x > r.sx - (r.ex - r.sx) && y < r.sy + (r.ex - r.sx) && y > r.sy - (r.ex - r.sx)) {
+				return i;
+			}
+		}
+		else if (r.ex < r.sx) {
+			if (x > r.ex && x < r.sx - (r.ex - r.sx) && y > r.sy + (r.ex - r.sx) && y < r.sy - (r.ex - r.sx)) {
+				return i;
+			}
 		}
 	}
 	return -1;
@@ -125,6 +142,19 @@ function drawRects() {
 		ctx.fillRect(r.sx, r.sy, r.ex - r.sx, r.ey - r.sy);
 		ctx.strokeRect(r.sx, r.sy, r.ex - r.sx, r.ey - r.sy);
 	}
+	for (i = 0; i < arCircle.length; i++) {
+		r = arCircle[i];
+		ctx.beginPath();
+		if (r.ex >= r.sx) {
+			ctx.arc(r.sx, r.sy, r.ex - r.sx, 0, 360);
+		}
+		else if (r.ex < r.sx){
+			ctx.arc(r.sx, r.sy, r.sx - r.ex, 0, 360);
+		}
+		ctx.fillStyle = r.color;
+		ctx.fill();
+		ctx.stroke();
+	}
 	for (i = 0; i < arStraight.length; i++) {
 		r = arStraight[i];
 		ctx.beginPath();
@@ -132,92 +162,92 @@ function drawRects() {
 		ctx.lineTo(r.ex, r.ey);
 		ctx.stroke();
 	}
-	for (i = 0; i < arCircle.length; i++) {
-		r = arCircle[i];
-		ctx.beginPath();
-		if (ex >= sx){
-			ctx.arc(sx, sy, ex - sx, 0, 2 * Math.PI);
-		}
-		else{
-			ctx.arc(sx, sy, sx - ex, 0, 2 * Math.PI);
-		}
-		ctx.stroke();
-		ctx.fillStyle = color;
-		ctx.fill();
-	}
+
 }
 
 ctx.strokeStyle = "black";
-ctx.lineWidth = 2;
+ctx.lineWidth = 4;
 color = "rgba(255, 255, 255, 0)"
 ctx.fillStyle = color;
 //액션
 canvas.onmousedown = function (e) {
 	e.preventDefault();
-	// 클릭한 좌표 구하고 그 위치에 도형이 있는지 조사
+	// 클릭한 좌표 구하기
 	sx = canvasX(e.clientX);
 	sy = canvasY(e.clientY);
 	// 도형을 클릭한 것이 아니면 그리기 시작
-	if (sq == true) {
+	if (sq) {
 		if (moving == -1) {
-			moving = getSquare(sx, sy);
 			squaredrawing = true;
 		}
+		//그 위치에 도형이 있는지 조사
+		moving = getSquare(sx, sy);
 	}
-	if (st == true) {
-		backup = ctx.getImageData(0, 0, canvas.width, canvas.height);
-		straightdrawing = true;
-	}
-	if (sc == true) {
+	if (sc) {
+		if (moving == -1) {
 			circledrawing = true;
+		}
+		moving = getCircle(sx, sy);
+	}
+	if (st) {
+		straightdrawing = true;
 	}
 }
 
 canvas.onmousemove = function (e) {
 	e.preventDefault();
-	// 화면 다시 그리고 현재 도형 그림
+	//마우스 좌표 구하기
 	ex = canvasX(e.clientX);
 	ey = canvasY(e.clientY);
+	// 화면 다시 그리고 현재 도형 그림
 	if (squaredrawing) {
 		drawRects();
 		ctx.fillStyle = color;
 		ctx.fillRect(sx, sy, ex - sx, ey - sy);
 		ctx.strokeRect(sx, sy, ex - sx, ey - sy);
+		if (moving != -1) {
+			let r = arSquare[moving];
+			r.sx += (ex - sx);
+			r.sy += (ey - sy);
+			r.ex += (ex - sx);
+			r.ey += (ey - sy);
+			sx = ex;
+			sy = ey;
+			drawRects();
+		}
 	}
-
+	if (circledrawing) {
+		drawRects();
+		ctx.beginPath();
+		if (ex >= sx) {
+			ctx.arc(sx, sy, ex - sx, 0, 360);
+		}
+		else if (ex < sx){
+			ctx.arc(sx, sy, sx - ex, 0, 360);
+		}
+		ctx.fillStyle = color;
+		ctx.fill();
+		ctx.stroke();
+		if (moving != -1) {
+			let r = arCircle[moving];
+			r.sx += (ex - sx);
+			r.sy += (ey - sy);
+			r.ex += (ex - sx);
+			r.ey += (ey - sy);
+			sx = ex;
+			sy = ey;
+			drawRects();
+		}
+	}
 	if (straightdrawing) {
-		ctx.putImageData(backup, 0, 0);
+		drawRects();
 		ctx.beginPath();
 		ctx.moveTo(sx, sy);
 		ctx.lineTo(ex, ey);
 		ctx.stroke();
 	}
-
-	if (circledrawing) {
-		drawRects();
-		ctx.beginPath();
-		if (ex >= sx){
-			ctx.arc(sx, sy, ex - sx, 0, 2 * Math.PI);
-		}
-		else{
-			ctx.arc(sx, sy, sx - ex, 0, 2 * Math.PI);
-		}
-		ctx.stroke();
-		ctx.fillStyle = color;
-		ctx.fill();
-	}
-
 	// 상대적인 마우스 이동 거리만큼 도형 이동
-	if (moving != -1) {
-		let r = arSquare[moving];
-		r.sx += (ex - sx);
-		r.sy += (ey - sy);
-		r.ex += (ex - sx);
-		r.ey += (ey - sy);
-		sx = ex;
-		sy = ey;
-		drawRects();
-	}
+
 }
 
 canvas.onmouseup = function (e) {
@@ -228,13 +258,8 @@ canvas.onmouseup = function (e) {
 		let x2 = Math.max(sx, ex);
 		let y2 = Math.max(sy, ey);
 		arSquare.push(new Square(x1, y1, x2, y2, color));
-	}
-	if (straightdrawing) {
-		let x1 = sx;
-		let y1 = sy;
-		let x2 = ex;
-		let y2 = ey;
-		arStraight.push(new Straight(x1, y1, x2, y2, color));
+		squaredrawing = false;
+		console.log(arSquare);
 	}
 	if (circledrawing) {
 		let x1 = sx;
@@ -242,10 +267,17 @@ canvas.onmouseup = function (e) {
 		let x2 = ex;
 		let y2 = ey;
 		arCircle.push(new Circle(x1, y1, x2, y2, color));
+		circledrawing = false;
+		console.log(arCircle);
 	}
-	squaredrawing = false;
-	straightdrawing = false;
-	circledrawing = false;
+	if (straightdrawing) {
+		let x1 = sx;
+		let y1 = sy;
+		let x2 = ex;
+		let y2 = ey;
+		arStraight.push(new Straight(x1, y1, x2, y2, color));
+		straightdrawing = false;
+	}
 	moving = -1;
 }
 //초기화
@@ -256,20 +288,19 @@ let 초기화 = document.getElementById("clear");
 	arStraight.length = 0;
 	arCircle.length = 0;
 }
-
+//마우스 좌표 구하기
 function canvasX(clientX) {
 	let bound = canvas.getBoundingClientRect();
 	let bw = 5;
 	return (clientX - bound.left - bw) * (canvas.width / (bound.width - bw * 2));
 }
-
 function canvasY(clientY) {
 	let bound = canvas.getBoundingClientRect();
 	let bw = 5;
 	return (clientY - bound.top - bw) * (canvas.height / (bound.height - bw * 2));
 }
 //색상
-selcolor.onchange = function(e) {
+selcolor.onchange = function (e) {
 	color = selcolor.value;
 }
 
